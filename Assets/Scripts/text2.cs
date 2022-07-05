fc39a2bdd7918ddbdf1297c552ca57bca5b55ffc
QinYi896 <1751479263@qq.com>
2022-07-05 21:22:06 +0800

    第二次修改

---
+++ Assets/Scripts/text2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class text2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("333333333");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
